5d6824fb32b0e9790f05cff150e7e307d0f8a579
vinnie <vrg6@zips.uakron.edu>
2022-04-27 00:02:41 -0400

    get kitty chat room looking ok

--- client-side-GUI.py
+++ client-side-GUI.py
@@ -8,6 +8,10 @@ from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 import time
 
+WIN_WIDTH = 40
+WIN_HEIGHT = 10
+TEXT_BOX_HEIGHT = 1.5
+
 def insert_tester(text_area):
     while True:
         text_area.insert(INSERT,"fart lol\n")
@@ -16,39 +20,23 @@ def insert_tester(text_area):
 
 
 # Creating tkinter main window
-win = Tk()
-win.title("ScrolledText Widget")
-  
-# Title Label
-'''
-Label(win, 
-          text = "ScrolledText Widget Example",
-          font = ("Times New Roman", 15), 
-          background = 'green', 
-          foreground = "white").grid(column = 0,
-                                     row = 0)
-'''
-# Creating scrolled text 
-# area widget
-text_area = ScrolledText(win, 
-                        wrap = WORD, 
-                        width = 40, 
-                        height = 10, 
-                        font = ("Times New Roman", 15))
-  
-text_area.grid(column = 0, pady = 10, padx = 10)
-text_area.insert(INSERT,
-"""\
-This is a scrolledtext widget to make tkinter text read only.
-Hi
-Geeks !!!
-Geeks !!!
-Geeks !!! 
-Geeks !!!
-Geeks !!!
-Geeks !!!
-Geeks !!!
-""")
-#start_new_thread(insert_tester, (text_area,))
+root = Tk()
+root.title("Kitty Chat")
+#root.geometry("600x400")
 
-win.mainloop()
+root.columnconfigure(0, weight=3)
+root.columnconfigure(1, weight=1)
+
+text_area = ScrolledText(root, wrap = WORD, font = ("Times New Roman", 15))
+text_area.grid(row=0, column=0, columnspan=1, sticky=N, padx=(10,0))
+
+
+input_box = Text(root, height=TEXT_BOX_HEIGHT)
+input_box.grid(row= 1, column = 0, sticky = W, padx=(10,0), pady=(10,10))
+
+send_button = Button(root, text="Send")
+send_button.grid(row=1, column=0, sticky= E, padx=50)
+
+#start_new_thread(insert_tester, (text_area,))
+root.resizable(False, False) 
+root.mainloop()
